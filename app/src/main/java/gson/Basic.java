39fbd2a3632ad450242af90aa46675f56853bb88
mr.shitou <bigmr.shitou@gmail.com>
2018-04-01 12:29:20 +0800

    加入数据库和表

---
+++ app/src/main/java/gson/Basic.java
@@ -0,0 +1,21 @@
+package gson;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Administrator on 2018/1/8.
+ */
+
+public class Basic {
+    @SerializedName("city")
+    public String cityName;
+
+    @SerializedName("id")
+    public String weatherId;
+    public Update update;
+    public class Update {
+
+        @SerializedName("loc")
+        public String updateTime;
+    }
+}
